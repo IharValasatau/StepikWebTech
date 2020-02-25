5990fd45156b65141cbeeffe4b3d2734961f1f8c
IharValasatau <ihar.valasatau@gmail.com>
2020-02-25 22:37:51 +0300

    Настройка WSGI сервера 2

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#  sudo /etc/init.d/gunicorn restart
+gunicorn -b 0.0.0.0:8080 hello:wsgi_app
 
-#sudo /etc/init.d/mysql start
+#  sudo /etc/init.d/mysql start
